b6bea66cba3525f61032c0fe80c4a8592e9163e9
Brian Blankenship <brianblankenship1978@gmail.com>
2020-11-28 21:49:27 -0500

    init

---
+++ easy_update.sh
@@ -0,0 +1,17 @@
+# Updating Observium to the latest version
+
+# Move the old software out of the way
+mv observium observium_old
+
+# Download the new software
+wget -Oobservium-community-latest.tar.gz https://www.observium.org/observium-community-latest.tar.gz
+tar zxvf observium-community-latest.tar.gz # Unpack it
+
+# Move the old data and config to the new software
+mv /opt/observium_old/rrd observium/; mv /opt/observium_old/logs observium/; mv /opt/observium_old/config.php observium/
+
+# Run discovery
+/opt/observium/discovery.php -u; /opt/observium/discovery.php -h all
+
+# Remove the old software
+rm /opt/observium_old
